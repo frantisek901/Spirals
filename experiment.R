18622ad86c15c84893f9c25fb9160054093b1708
František Kalvas <kalvas@kss.zcu.cz>
2022-03-04 07:35:13 +0100

    non-identity data loading and regression added

--- experiment.R
+++ experiment.R
@@ -45,7 +45,8 @@ test = read_csv("speedTestingData.csv", skip = 6)
 wrng = read_csv("wronglySpecifiedExperiment.csv", skip = 6)
 
 res = read_csv("experiment01part01.csv", skip = 6) %>%
-  add_row(read_csv("experiment01part02.csv", skip = 6))
+  add_row(read_csv("experiment01part02.csv", skip = 6)) %>%
+  add_row(read_csv("experiment01part03.csv", skip = 6))
 
 
 
@@ -63,6 +64,15 @@ ggplot(res, aes(y = normalized_polarization_final , x = boundary)) + geom_jitter
 ggplot(res, aes(y = normalized_polarization_final , x = mode)) + geom_boxplot(alpha = 0.2) + geom_jitter()
 ggplot(res, aes(y = normalized_polarization_final , x = mode, col = as.factor(boundary))) + geom_boxplot(alpha = 0.2) + geom_jitter()
 ggplot(res, aes(y = normalized_polarization_final , x = mode, col = as.factor(id_threshold))) + geom_jitter(alpha = 0.5, size = 2) + theme_minimal()
+
+ggplot(res, aes(x = RS)) + geom_bar()
+
+
+
+# Some regressions --------------------------------------------------------
+
+summary(lm(normalized_polarization_final~ESBSG_polarization_final,res))
+
 
 
 
