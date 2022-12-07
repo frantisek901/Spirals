f308341aaff4ff5d139688ec3f6d433bffded67c
František Kalvas <kalvas@kss.zcu.cz>
2022-12-07 19:33:33 +0100

    Update step2.R

--- IdentityPaper/step2.R
+++ IdentityPaper/step2.R
@@ -22,6 +22,7 @@ library(dplyr)
 library(tibble)
 library(forcats)
 library(ggplot2)
+library(rstatix)
 
 
 
@@ -47,5 +48,91 @@ ts = raw %>%
   mutate(
     across(.cols = c(1:2, 4), factor),
     even_N = ((N %%2) == 0))
+
+
+# Summary statistics ------------------------------------------------------
+
+# Basic
+ts %>% get_summary_stats(ESBG, extremness, diversity)
+
+# According all factors:
+for (i in c(1, 5:9, 14)) {
+  print(names(ts)[i])
+  ts %>%
+    group_by(eval(str2lang(names(ts)[i]))) %>%
+    get_summary_stats(ESBG, extremness, diversity) %>%
+    print()
+
+}
+
+
+# Regression --------------------------------------------------------------
+
+# Regression just on classical conditions of HK
+m1 = lm(ESBG ~ Boundary + Conformity + even_N + HK_distribution, ts)
+m2 = lm(extremness ~ Boundary + Conformity + even_N + HK_distribution, ts)
+m3 = lm(diversity ~ Boundary + Conformity + even_N + HK_distribution, ts)
+stargazer::stargazer(m1, m2, m3, type = "text")
+
+m21 = lm(ESBG ~ factor(Boundary) + factor(Conformity) + even_N + HK_distribution, ts)
+m22 = lm(extremness ~ factor(Boundary) + factor(Conformity) + even_N + HK_distribution, ts)
+m23 = lm(diversity ~ factor(Boundary) + factor(Conformity) + even_N + HK_distribution, ts)
+stargazer::stargazer(m21, m22, m23, type = "text")
+
+m11 = lm(ESBG ~ Boundary * Conformity * even_N * HK_distribution, ts)
+m12 = lm(extremness ~ Boundary * Conformity * even_N * HK_distribution, ts)
+m13 = lm(diversity ~ Boundary * Conformity * even_N * HK_distribution, ts)
+stargazer::stargazer(m11, m12, m13, type = "text")
+
+stargazer::stargazer(m1, m11, m21, type = "text")
+stargazer::stargazer(m2, m12, m22, type = "text")
+stargazer::stargazer(m3, m13, m23, type = "text")
+
+m31 = lm(ESBG ~ Boundary + Boundary_STD + Conformity + Conformity_STD + even_N + HK_distribution, ts)
+m32 = lm(extremness ~ Boundary + Boundary_STD + Conformity + Conformity_STD + even_N + HK_distribution, ts)
+m33 = lm(diversity ~ Boundary + Boundary_STD + Conformity + Conformity_STD + even_N + HK_distribution, ts)
+stargazer::stargazer(m31, m32, m33, type = "text")
+
+m41 = lm(ESBG ~ Boundary * Boundary_STD + Conformity * Conformity_STD + even_N + HK_distribution, ts)
+m42 = lm(extremness ~ Boundary * Boundary_STD + Conformity * Conformity_STD + even_N + HK_distribution, ts)
+m43 = lm(diversity ~ Boundary * Boundary_STD + Conformity * Conformity_STD + even_N + HK_distribution, ts)
+stargazer::stargazer(m41, m42, m43, type = "text")
+
+stargazer::stargazer(m1, m31, m41, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m2, m32, m42, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m3, m33, m43, type = "text", omit.stat = c("f", "ser"))
+
+m51 = lm(ESBG ~ factor(Boundary) * factor(Boundary_STD) + factor(Conformity) * factor(Conformity_STD) + even_N + HK_distribution, ts)
+m52 = lm(extremness ~ factor(Boundary) * factor(Boundary_STD) + factor(Conformity) * factor(Conformity_STD) + even_N + HK_distribution, ts)
+m53 = lm(diversity ~ factor(Boundary) * factor(Boundary_STD) + factor(Conformity) * factor(Conformity_STD) + even_N + HK_distribution, ts)
+stargazer::stargazer(m51, m52, m53, type = "text")
+
+m61 = lm(ESBG ~ factor(Conformity) * factor(Boundary) + factor(Conformity_STD) * factor(Boundary_STD) + even_N + HK_distribution, ts)
+m62 = lm(extremness ~ factor(Conformity) * factor(Boundary) + factor(Conformity_STD) * factor(Boundary_STD) + even_N + HK_distribution, ts)
+m63 = lm(diversity ~ factor(Conformity) * factor(Boundary) + factor(Conformity_STD) * factor(Boundary_STD) + even_N + HK_distribution, ts)
+stargazer::stargazer(m61, m62, m63, type = "text")
+
+# Just short glance that 'almost full' model address 'only' cca 8pctp more:
+stargazer::stargazer(m1, m31, m41, m51, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m2, m32, m42, m52, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m3, m33, m43, m53, type = "text", omit.stat = c("f", "ser"))
+
+m71 = lm(ESBG ~ factor(Boundary) * factor(Boundary_STD), ts)
+m72 = lm(extremness ~ factor(Boundary) * factor(Boundary_STD), ts)
+m73 = lm(diversity ~ factor(Boundary) * factor(Boundary_STD), ts)
+stargazer::stargazer(m71, m72, m73, type = "text")
+
+m81 = lm(ESBG ~ (Boundary) * (Boundary_STD), ts)
+m82 = lm(extremness ~ (Boundary) * (Boundary_STD), ts)
+m83 = lm(diversity ~ (Boundary) * (Boundary_STD), ts)
+stargazer::stargazer(m81, m82, m83, type = "text")
+
+stargazer::stargazer(m41, m71, m81, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m42, m72, m82, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m43, m73, m83, type = "text", omit.stat = c("f", "ser"))
+
+stargazer::stargazer(m1, m31, m41, m81, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m2, m32, m42, m82, type = "text", omit.stat = c("f", "ser"))
+stargazer::stargazer(m3, m33, m43, m83, type = "text", omit.stat = c("f", "ser"))
 
 
